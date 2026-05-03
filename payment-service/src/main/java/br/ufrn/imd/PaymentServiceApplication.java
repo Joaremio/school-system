c33bcdee4c548a538eaa56fc58bee97a67bb0d8e
Joaremio <joaremiorevoredo@gmail.com>
2026-05-03 18:58:41 -0300

    chore: setup inicial do payment-service

---
+++ payment-service/src/main/java/br/ufrn/imd/PaymentServiceApplication.java
@@ -0,0 +1,17 @@
+package br.ufrn.imd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableFeignClients
+@EnableScheduling
+@SpringBootApplication
+public class PaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentServiceApplication.class, args);
+	}
+
+}
